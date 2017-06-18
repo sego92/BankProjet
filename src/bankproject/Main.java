337b875141073495c2a87de70320b1bdba49da37
sego92 <sego.maris@gmail.com>
2017-06-18 19:01:11 +0200

    console

--- src/bankproject/Main.java
+++ src/bankproject/Main.java
@@ -2,7 +2,11 @@ package bankproject;
 
 
 import java.sql.SQLException;
+import java.util.Scanner;
 
+import bankproject.entities.Account;
+import bankproject.entities.Customer;
+import bankproject.entities.Operation;
 import bankproject.readers.*;
 import bankproject.services.SQLiteManager;
 import bankproject.services.SrvAccount;
@@ -29,6 +33,29 @@ public class Main {
 		yyy.start();
 		xxx.start();
 		
+
+		for (;;){
+			
+			Scanner sc = new Scanner (System.in);
+			System.out.println("Veuillez saisir un lastName :");
+			String sLastname = sc.nextLine();
+			System.out.println("Veuillez saisir un firstName :");
+			String sFirstName = sc.nextLine();
+			try {
+				Customer cu = SrvCustomer.getINSTANCE().get(sLastname,sFirstName);
+				if (cu==null){
+					System.out.println("cette persoone n'a pas de compte dans cette banque");
+				}else {
+					Account ac = (Account) SrvAccount.getINSTANCE().get(cu.getId());
+					System.out.println("AccountNumber =" + ac.getAccountNumber());
+					Operation op = (Operation) SrvOperation.getINSTANCE().get(ac.getId());
+					System.out.println("Credit/Debit = " + op.getCreditDebit());
+				}
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 		
 
 	}
